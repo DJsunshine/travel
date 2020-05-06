d1dd7782980dd870ec9a10cf6966783703bbaf81
DJsunshine <1159397792@qq.com>
2020-05-06 11:33:04 +0800

    add header

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from "./components/Header"
+export default {
+  // import引入的组件需要注入到对象中才能使用
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+};
+</script>
+
+<style lang='stylus' scoped>
+</style>
