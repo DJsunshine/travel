687f5130f89bd53d7bd9027b975a14e5d6cbe8be
DJsunshine <1159397792@qq.com>
2020-05-06 16:28:39 +0800

    change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="header">
     <home-header></home-header>
+    <home-swiper></home-swiper>
+    <div>test</div>
   </div>
 </template>
 
 <script>
 import HomeHeader from "./components/Header"
+import HomeSwiper from "./components/Swiper"
+
 export default {
   // import引入的组件需要注入到对象中才能使用
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 };
 </script>
