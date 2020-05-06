d0795aeb11cc01c79464253795558c27479544be
DJsunshine <1159397792@qq.com>
2020-05-06 18:13:17 +0800

    add icons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,20 +2,23 @@
   <div class="header">
     <home-header></home-header>
     <home-swiper></home-swiper>
-    <div>test</div>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from "./components/Header"
 import HomeSwiper from "./components/Swiper"
+import HomeIcons from "./components/Icons"
+
 
 export default {
   // import引入的组件需要注入到对象中才能使用
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 };
 </script>
